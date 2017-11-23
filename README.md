a3765c1ded0c5f12425cdc63de3dbe119adddc12
gohanwotabe <gohanwotabe@gmail.com>
2017-11-23 17:42:43 +0800

    rewrite again

--- README.md
+++ README.md
@@ -3,16 +3,58 @@ SOCKS v4/v4a/v5 server implementation with user/pass authentication node.js
 
 A simple SOCKS v5/v4/v4a server implementation and demo proxy.
 
-You can run it easily as::
+You can run it easily as:
 
-  node proxy.js
+  node proxy.js [options]
 
-This will create a proxy at ``127.0.0.1`` on port ``8888``.
+This will create a proxy defaults at `127.0.0.1:8888`.
+`options`:see `node proxy.js --help`
 
 You can use this as a good starting point for writing a proxy or a tunnel!
 
 ### Install
 - npm install socks5server
+
+### Use the server in your project
+
+```
+const socks5server=require('socks5server');
+
+var server=socks5server.createServer();
+//or
+var server=new socks5server.socksServer();
+```
+
+
+### Implementations
+
+✅:OK
+❌:not implemented
+❓:i don't kown
+
+#### Socks4
+* user 					✅
+
+#### Socks4a
+* DNS 					✅
+
+#### Socks5
+* address
+	* ipv4				✅
+	* ipv6				✅
+	* domain name		✅
+
+* auth methods
+	* no auth 			✅
+	* userpass 			✅
+	* GSSAPI 			❌
+	* iana assigned		❌
+	* private methods	✅
+
+* protocol
+	* tcp				✅
+	* udp				❌ (hope if someone can help)
+	
 
 
 ### License
--- proxy.js
+++ proxy.js
@@ -2,24 +2,36 @@
 var net = require('net'),
 	socks = require('./socks.js'),
 	info = console.log.bind(console);
+var commander = require('commander');
+	
+commander
+	.usage('[options]')
+	.option('-u, --user [value]', 'set a user:pass format user')
+	.option('-H, --host [value]', 'host to listen,defaults to 127.0.0.1')
+	.option('-P, --port <n>', 'host to listen,defaults to 1080',/^\d+$/i)
+	.option('-L, --localAddress [value]', 'local assress to send the connection')
+	.parse(process.argv);
 
 // Create server
 // The server accepts SOCKS connections. This particular server acts as a proxy.
-const users={};
-if(process.argv[3]&&process.argv[4]){
-	users[process.argv[3]]=process.argv[4];
-}
-
-var HOST='127.0.0.1',
-	PORT='8888',
+var HOST=commander.host||'127.0.0.1',
+	PORT=commander.port||'8888',
 	server = socks.createServer();
 
-server.on('socket',(socket, port, address, proxy_ready)=>{
+info('server started at ',HOST,':',PORT);
+
+if(commander.user){
+	let u=commander.user.split(":");
+	server.setSocks5UserPass(u[0],u[1]);
+	info('user ',commander.user);
+}
+
+
+server.on('socket',(socket, port, address, protocol, proxy_ready)=>{
 
 	// Implement your own proxy here! Do encryption, tunnelling, whatever! Go flippin' mental!
 	// I plan to tunnel everything including SSH over an HTTP tunnel. For now, though, here is the plain proxy:
-
-	var proxy = net.createConnection({port:port, host:address,localAddress:process.argv[2]||undefined}, proxy_ready);
+	var proxy = net.createConnection({port:port, host:address,localAddress:commander.localAddress||undefined}, proxy_ready);
 	var localAddress,localPort;
 	proxy.on('connect',()=>{
 		info('%s:%d <== %s:%d ==> %s:%d',socket.remoteAddress,socket.remotePort,
@@ -29,27 +41,24 @@ server.on('socket',(socket, port, address, proxy_ready)=>{
 		proxy.pipe(socket);
 		socket.pipe(proxy);
 		proxy.on('error',e=>{
-			console.error('connection error:',e);
-			server.emit('connection_error',e);
+			console.error('connection error:',e.message);
+			server.emit('connection_error',e.message);
 		});
-		socket.on('error',e=>{
-			console.error('proxy error:',e);
-			server.emit('proxy_error',e);
-		});
+	}).on('error',e=>{
+		console.error('proxy error:',e.message);
 	});
 	 
-	proxy.on('close', function(had_error) {
-		try {
+	socket.on('close', function(had_error) {
+		try{
 			if(localAddress && localPort)
 				console.log('The proxy %s:%d closed', localAddress, localPort);
 		else 
 			console.error('Connect to %s:%d failed', address, port);
-			socket.end();
-		} catch (err) {}
+		}catch(err) {}
 	});
 }).on('error', function (e) {
 	console.error('SERVER ERROR: %j', e);
-	if (e.code == 'EADDRINUSE') {
+	if(e.code == 'EADDRINUSE') {
 		console.log('Address in use, retrying in 10 seconds...');
 		setTimeout(function () {
 			console.log('Reconnecting to %s:%s', HOST, PORT);
@@ -57,6 +66,9 @@ server.on('socket',(socket, port, address, proxy_ready)=>{
 			server.listen(PORT, HOST);
 		}, 10000);
 	}
+}).on('socket_error',(socket,e)=>{
+	console.error('socket error:',e);
+}).on('socks_error',(socket,e)=>{
+	console.error('socks error:',e);
 }).listen(PORT, HOST);
 
-// vim: set filetype=javascript syntax=javascript :
